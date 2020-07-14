c56d7d2512b1e482793b2093116534cde6af8e24
kimdonghyeok1352 <vvvv1352@naver.com>
2020-07-14 10:36:42 +0900

    Java 2020.07.14(2)

--- Test01/src/A01.java
+++ Test01/src/A01.java
@@ -1,20 +1,69 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
-class Food{
-	String menu;
-	int price;
-	Food(String menu){
-		this.menu=menu;
-		price = 10000;
+
+class Cook {// 요리쓰레드
+	String food;
+	boolean send = false;
+
+	void set(String f) {
+		food = f;
+		send = true;
+
+		synchronized (this) {
+			notifyAll();// 잠자고 있는 쓰레드 다 깨운다
+
+		}
+
 	}
-	public String toString() {
-		return menu;
+	String get() {
+		if(send == false) {
+			try {
+				synchronized (this) {
+					wait();//손님이 기다림
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return food;
+	}
+}
+class Chef extends Thread{
+	Cook c;
+	Chef(Cook c){
+		this.c=c;
+	}
+	public void run() {
+		c.set("음식");
+	}
+}
+//hello
+class Custumer extends Thread{
+	Cook c;
+	Custumer(Cook c){
+		this.c=c;
+	}
+	public void run() {
+		System.out.println(c.get());
 	}
 }
 public class A01 {
 
 	public static void main(String[] args) {
-			Food f = new Food("짜장면");
-			System.out.println(f+" "+f.price+"원");
+		Cook co = new Cook();
+		Custumer c1 = new Custumer(co);
+		Custumer c2 = new Custumer(co);
+		Chef c3 = new Chef(co);
+		
+		try {
+			c1.start();
+			c2.start();
+			Thread.sleep(1000);
+			c3.start();
+			
+			c1.join();
+			c2.join();
+			c3.join();
+		}catch(Exception e) {}
 	}
 }
