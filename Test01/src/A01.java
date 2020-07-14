676e83fb25cd07755c1f0f719d9bd65eff86cc83
kimdonghyeok1352 <vvvv1352@naver.com>
2020-07-14 11:32:19 +0900

    2020-07-14(5)

--- Test01/src/A01.java
+++ Test01/src/A01.java
@@ -117,7 +117,7 @@ class Calcu implements Cal {
 	}
 
 }*/
-class Th extends Thread{
+/*class Th extends Thread{
 	String str;
 	Th(String str){
 		this.str=str;
@@ -134,4 +134,106 @@ public class A01 {
 		th.start();
 
 	}
+}*/
+/*class Timer extends Thread{
+	public void run() {
+		for(int i = 0; i<10; i++) {
+			System.out.println(i);
+			try {
+				sleep(1000);
+			}catch(Exception e) {}
+		}
+	}
 }
+public class A01 {
+
+	public static void main(String[] args) {
+		Timer t = new Timer();
+		
+		t.start(); 
+		   
+
+	}
+}*/
+/*class Total {
+	int sum;
+
+	Total() {
+		sum = 0;
+	}
+
+	void total(int n) {
+		sum += n;
+	}
+
+	int get() {
+		return sum;
+	}
+}
+class AThread extends Thread{
+	Total t;
+	int a,b;
+	int sum=0;
+	AThread(Total t,int a, int b){
+		this.t=t;
+		this.a=a;
+		this.b=b;
+	}
+	public void run() {
+		for(int i=a; i<=b; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		
+	}
+}
+public class A01 {
+
+	public static void main(String[] args) {
+		Total t = new Total();
+		AThread a = new AThread(t, 1, 50);
+		AThread b = new AThread(t, 51, 100);
+		a.start();
+		b.start();
+		try {
+			a.join();
+			b.join();
+		}catch(Exception e) {}
+
+	}
+}*/
+abstract class Figure{
+	int a,b;
+	abstract void circle_area();
+	abstract void rec_area();
+}
+	
+class Circle extends Figure{
+	Circle(int a){
+		this.a=a;
+	}
+	void circle_area() {
+		System.out.println(a*a*3.14);
+	}
+}
+class Rec extends Figure{
+	Rec(int a, int b){
+		this.a=a;
+		this.b=b;
+	}
+	void rec_area() {
+		System.out.println(a*b);
+	}
+}
+public class A01 {
+
+	public static void main(String[] args) {
+		Figure f1=new Circle(5);
+		Figure f2=new Rec(2,5); 
+		f1.circle_area(); 
+		f2.rec_area();
+
+	
+	}
+}
+	
