a77c1978f087d7391f59efd5dd23137351f2a801
kimdonghyeok1352 <68038732+kimdonghyeok1352@users.noreply.github.com>
2020-08-14 12:53:21 +0900

    20200814

--- Test01/src/프로그래머스.java
+++ Test01/src/프로그래머스.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
+class Solution {
+	int n;
+	String s = "수";
+	String b = "박";
+	String d = "";
+
+	public String solution(int n) {
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0) {
+				d += s;
+			} else {
+				d += b;
+			}
+		}
+		return d;
+	}
+}
+
 public class 프로그래머스 {
 
 	public static void main(String[] args) {
-		String a = "수박";
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		if(n%2==0) {
-			for(int i =1; i<=n-1; i ++) {
-				System.out.println(a);
-			}
-		}
+		Solution s = new Solution();
+		System.out.println(s.solution(3));
+		
 	}
-
+	
 }
